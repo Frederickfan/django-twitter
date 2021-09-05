9115a13b6943fb44e96e7b5b777387cef29f0602
Frederick <zf2056@nyu.edu>
2021-09-04 21:51:40 -0400

    Finished follow and unfollow, and also add unit tests for all features in Friendships module.

--- friendships/admin.py
+++ friendships/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from friendships.models import Friendship
+
+
+@admin.register(Friendship)
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ('id', 'from_user', 'to_user', 'created_at')
+    date_hierarchy = 'created_at'
