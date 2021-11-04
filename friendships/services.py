009e8bf56cff6d6bb22f882e1d0278f90ca834bb
Frederick <zf2056@nyu.edu>
2021-11-03 20:38:23 -0400

    newsfeed model

---
+++ friendships/services.py
@@ -0,0 +1,11 @@
+from friendships.models import Friendship
+
+
+class FriendshipService:
+
+    @classmethod
+    def get_followers(cls, user):
+        friendships = Friendship.objects.filter(
+            to_user=user,
+        ).prefetch_related('from_user')
+        return [friendship.from_user for friendship in friendships]
--- twitter/urls.py
+++ twitter/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 from rest_framework import routers
 from accounts.api.views import UserViewSet, AccountViewSet
 from friendships.api.views import FriendshipViewSet
+from newsfeeds.api.views import NewsFeedViewSet
 from tweets.api.views import TweetViewSet
 
 router = routers.DefaultRouter()
@@ -25,6 +26,7 @@ router.register(r'api/users', UserViewSet)
 router.register(r'api/accounts', AccountViewSet, basename='accounts')
 router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
+router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
