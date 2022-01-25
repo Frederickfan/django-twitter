6884c1d0c9c072362facbf56fff1e17d3cc06410
Frederick <zf2056@nyu.edu>
2022-01-24 21:25:11 -0500

    Create api for comments

---
+++ comments/api/serializers.py
@@ -0,0 +1,55 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
+from accounts.api.serializers import UserSerializer
+from comments.models import Comment
+from tweets.models import Tweet
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Comment
+        fields = (
+            'id',
+            'tweet_id',
+            'user',
+            'content',
+            'created_at',
+            'updated_at',
+        )
+
+
+class CommentSerializerForCreate(serializers.ModelSerializer):
+    tweet_id = serializers.IntegerField()
+    user_id = serializers.IntegerField()
+    """
+    命名习惯：
+
+    大写开头 User 代表 name of Model (User Model)
+    user is an instance of User
+    user_id is the primary key of user
+
+    再比如： 
+    Comment is the name of the Comment Model 
+    comment_instance 
+    comment_id
+    """
+
+    class Meta:
+        model = Comment
+        fields = ('content', 'tweet_id', 'user_id')
+
+    def validate(self, data):
+        tweet_id = data['tweet_id']
+        if not Tweet.objects.filter(id=tweet_id).exists():
+            raise ValidationError({'message': 'tweet does not exist!'})
+        return data
+
+    def create(self, validated_data):
+        return Comment.objects.create(
+            user_id=validated_data['user_id'],
+            tweet_id=validated_data['tweet_id'],
+            content=validated_data['content'],
+        )
