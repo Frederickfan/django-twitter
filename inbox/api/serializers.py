4691afaf2c055ace046773bc853dc98f8ad66dd1
Frederick <zf2056@nyu.edu>
2022-02-17 21:37:53 -0500

    notification api finished and unit test finished

---
+++ inbox/api/serializers.py
@@ -0,0 +1,43 @@
+from rest_framework import serializers
+from notifications.models import Notification
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    """
+    User1 followed you(receipient)
+    actor = User1
+    verb = "Followed"
+
+    user1 liked your tweet
+    actor = user1
+    target = tweet1
+    verb = "like"
+
+    """
+
+    class Meta:
+        model = Notification
+        fields = (
+            'id',
+            'actor_content_type',
+            'actor_object_id',
+            'verb',
+            'action_object_content_type',
+            'action_object_object_id',
+            'target_content_type',
+            'target_object_id',
+            'timestamp',
+            'unread',
+        )
+
+class NotificationSerializerForUpdate(serializers.ModelSerializer):
+    unread = serializers.BooleanField()
+
+    class Meta:
+        model = Notification
+        fields = ('unread',)
+
+    def update(self, instance, validated_data):
+        instance.unread = validated_data['unread']
+        instance.save()
+        return instance
--- twitter/urls.py
+++ twitter/urls.py
@@ -22,6 +22,7 @@ from likes.api.views import LikeViewSet
 from newsfeeds.api.views import NewsFeedViewSet
 from tweets.api.views import TweetViewSet
 from comments.api.views import CommentViewSet
+from inbox.api.views import NotificationViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', UserViewSet)
@@ -31,6 +32,7 @@ router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 router.register(r'api/comments', CommentViewSet, basename='comments')
 router.register(r'api/likes', LikeViewSet, basename='likes')
+router.register(r'api/notifications', NotificationViewSet, basename='notification')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
